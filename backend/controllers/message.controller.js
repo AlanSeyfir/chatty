16ded9bf1d08c2acb03aa9bd51dfc50c874b10b3
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-14 15:15:54 -0600

    Fix typo and add get messages endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -8,12 +8,12 @@ export const sendMessage = async (req, res) => {
     const senderId = req.user._id;
 
     let conversation = await Conversation.findOne({
-      partipants: { $all: [senderId, receiverId] },
+      participants: { $all: [senderId, receiverId] },
     });
 
     if (!conversation) {
       conversation = await Conversation.create({
-        partipants: [senderId, receiverId],
+        participants: [senderId, receiverId],
       });
     }
 
@@ -32,6 +32,27 @@ export const sendMessage = async (req, res) => {
     res.status(201).json(newMessage);
   } catch (error) {
     console.log('Error in sendMessage controller: ', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate('messages'); //NOT REFERENCE BUT ACTUAL MESSAGES
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+    console.log('Here are messages: ' + messages);
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log('Error in getMessages controller: ', error.message);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
