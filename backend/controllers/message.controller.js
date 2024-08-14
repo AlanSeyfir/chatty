0482a2e17d09fcf2b5f81af3c49273348e916702
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-13 23:10:33 -0600

    hot fix

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -3,6 +3,7 @@ export const sendMessage = async (req, res) => {
     const { message } = req.body;
     const { id } = req.params.id;
     const senderId = req.userId;
+    console.log('MESSAGE CORRECT SENT');
   } catch (error) {
     console.log('Error in sendMessage controller: ', error.message);
     res.status(500).json({ error: 'Internal server error' });
