a72e27fe269614bde3af0d5cd98074bc88a8e0ce
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-15 09:54:02 -0600

    Fix message input

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,4 +1,3 @@
-import e from 'express';
 import { useState } from 'react';
 import { BsSend } from 'react-icons/bs';
 import useSendMessage from '../../hooks/useSendMessage';
@@ -8,7 +7,7 @@ const MessagesInput = () => {
 
   const { loading, sendMessage } = useSendMessage();
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!message) return;
 
@@ -23,6 +22,8 @@ const MessagesInput = () => {
           type="text"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
           placeholder="Send a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button
           type="submit"
