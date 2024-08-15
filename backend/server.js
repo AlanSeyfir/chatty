53eddf49f0b6d32af2ea1d443cfd1c5e920056c0
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-15 12:40:43 -0600

    build to production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 //MIDDLEWARES - ORDER MATTERS
 app.use(express.json());
 app.use(cookieParser());
@@ -21,12 +24,13 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port: http://localhost:${PORT}`);
 });
-
-// app.get('/', (req, res) => {
-//   //root route http://localhost:5000/
-//   res.send('hello dld');
-// });
