2078ce1e66c9a3340da89e59674188d228cafa8e
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-15 13:26:41 -0600

    Add title and icon, change colors

---
+++ TODOS.md
@@ -0,0 +1,11 @@
+# TODOS
+
+#### NICE TO HAVE
+
+- Enhanced UI
+-
+
+#### Bugs
+
+- When the receiver writes on the search input. If both users send at the same time it will appear on the search user the message. But it will still appear on the original receiver.
+- Max width on the messages
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -37,7 +37,10 @@ const SearchInput = () => {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+      <button
+        type="submit"
+        className="btn btn-circle bg-indigo-700 text-white border-indigo-700"
+      >
         <IoSearchSharp className="w-6 h-6 outline-none" />
       </button>
     </form>
