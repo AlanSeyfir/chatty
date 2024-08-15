c493ef13801f03301117261ca3d867d1bb886956
AlanSeyfir <seyfirdeveloper@gmail.com>
2024-08-15 11:44:32 -0600

    Add socket io and real time msg

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,11 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
-const app = express();
+import { app, server } from './socket/socket.js';
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
-
-app.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server running on port: http://localhost:${PORT}`);
-});
 
 //MIDDLEWARES - ORDER MATTERS
 app.use(express.json());
@@ -25,6 +21,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
 
 // app.get('/', (req, res) => {
 //   //root route http://localhost:5000/
